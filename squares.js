9fdbe620c33ef0a7322289f62afb780417ad58f8
Elliot P. <elliotjpc98@gmail.com>
2026-04-23 23:38:47 -0400

    1. Function That Adds A Square

--- squares.js
+++ squares.js
@@ -1,7 +1,8 @@
 let squareArea = document.querySelector("#squarearea");
 // let squareArea = document.getElementById("squarearea");
 let squareCount = parseInt(Math.random()*21)+30; //30-50
-
+//Make some variable for a really big zIndex
+document.querySelectorAll("button")[0].onclick= addSquare;
 for(let i = 0;i<squareCount;i++){
     //make a square
     let square = document.createElement("div");
@@ -9,7 +10,16 @@ for(let i = 0;i<squareCount;i++){
     square.style.left = parseInt(Math.random()*650)+"px";
     square.style.top = parseInt(Math.random()*250)+"px";
     square.style.backgroundColor = getRandomColor();
+    //I can put a onclick and a function(this)
     //put it on the screen
+    squareArea.append(square);
+}
+function addSquare(){
+    let square = document.createElement("div");
+    square.className = "square";
+    square.style.left = parseInt(Math.random()*650)+"px";
+    square.style.top = parseInt(Math.random()*250)+"px";
+    square.style.backgroundColor = getRandomColor();
     squareArea.append(square);
 }
 
@@ -22,4 +32,15 @@ function getRandomColor(){
     }
     //return that string
     return color;
+}
+//Make a function to add 1 square
+//Make sure that I set that as a the oneclick for thebutton
+
+//Make a Function that changes all the colors of all the squares
+//Make sure the button calls it
+function changeColors(){
+    //Make an array
+    let allSquares = document.querySelectorAll(".square");
+    //For of loop or just do a regular for loop with index
+    //Change the color
 }
